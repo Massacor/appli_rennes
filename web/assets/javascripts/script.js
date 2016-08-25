d030aa65e8da7b6a90fb4b706dd21cba0add4c62
[Massacor] <[apowarp@gmail.com]>
2016-08-25 16:44:20 +0200

    Managed to create the collapse with Jquery. Didn't work as intended, going to make it with animation CSS

--- web/assets/javascripts/script.js
+++ web/assets/javascripts/script.js
@@ -1,17 +1,58 @@
-$(function() {
+$(document).ready(function(){
 
-    var start = moment().subtract(7, 'days');
-    var end = moment();
 
-    function cb(start, end) {
-        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    }
+  /* ===============
+      Date picker
+  ================ */
 
-    $('#reportrange').daterangepicker({
-        startDate: start,
-        endDate: end,
-    }, cb);
 
-    cb(start, end);
+  $(function() {
+
+      var start = moment().subtract(7, 'days');
+      var end = moment();
+
+      function cb(start, end) {
+          $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+      }
+
+      $('#reportrange').daterangepicker({
+          startDate: start,
+          endDate: end,
+      }, cb);
+
+      cb(start, end);
+  });
+
+
+
+
+  /* =============
+      Homemade collapse
+  ============== */
+
+  $("#content_test1-trigger").click(function(){
+    $("#content_test1").toggle(500, function(){
+      $("#content_test1-trigger").toggleClass("glyphicon-chevron-down glyphicon-chevron-up")
+    });
+  });
+
+  $("#content_test2-trigger").click(function(){
+    $("#content_test2").toggle(300);
+  });
+
+  $("#content_test3-trigger").click(function(){
+    $("#content_test3").hide(300);
+  });
+
+  $("#content_test4-trigger").click(function(){
+    $("#content_test4").hide(300);
+  });
+
+  $("#content_test4-trigger").click(function(){
+    $("#content_test4").hide(300);
+  });
+
+
+
 
 });
